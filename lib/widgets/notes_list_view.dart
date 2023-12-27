47f1932d62b657647f02d27d724bf855a9cab4aa
zeadkhaledlE <zeadeess@gmail.com>
2023-12-27 17:29:02 +0200

    add floating action button

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -3,13 +3,13 @@ import 'package:notes_app/widgets/custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
-
+  final data = const [];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8),
+          padding: EdgeInsets.symmetric(vertical: 4),
           child: NoteItem(),
         );
       },
