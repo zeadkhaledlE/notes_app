e65a7415a2ba4dff7bb8a9bc59ae751b3e4227af
zeadkhaledlE <zeadeess@gmail.com>
2023-12-28 15:50:03 +0200

    show modal bottom sheet

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -6,13 +6,17 @@ class NotesListView extends StatelessWidget {
   final data = const [];
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 4),
-          child: NoteItem(),
-        );
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 4),
+            child: NoteItem(),
+          );
+        },
+      ),
     );
   }
 }
