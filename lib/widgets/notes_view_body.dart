e93c0e07403bc93f3b6e4ed8d7e35d293d2f0009
zeadkhaledlE <zeadeess@gmail.com>
2024-01-05 14:43:14 +0200

    create edit note view

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -15,7 +15,9 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
-          Expanded(child: NotesListView()),
+          Expanded(
+            child: NotesListView(),
+          ),
         ],
       ),
     );
